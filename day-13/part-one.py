490d8877eb0904bb12717a748cffb761b368db9d
Megan O'Keefe <megan037@gmail.com>
2022-12-13 10:07:49 -0500

    i'm def making this too complicated....

--- day-13/part-one.py
+++ day-13/part-one.py
@@ -1,43 +1,7 @@
-"""
-task: reorder a list of packets to decode a message. 
-
-given a list of pairs of packets: how many are in the right order? 
-
-each list starts with [, ends with ] and contains 0 or more comma separated values, either:
-     an integer 
-     or another list 
-
-you could see [7,7,7,7]  ("regular list")
-or [[8,7,6]]  ("nested with many shell layers")
-or [[1],[2,3,4]]  ("nested separate")
-or [1,[2,[3,[4,[5,6,7]]]],8,9]  ("nested populated)
-or [] ("Regular empty list")
-or [[[]]] ("empty nested list")
-
-given a pair that looks like: [1,1,3,1,1] vs [1,1,5,1,1] 
-you iterate left to right and compare 0th to 0th, and so on.  ("left" vs "right")
-
-
-if both vals are ints: 
-    lower int must come before right. so if left < right, ✅ RIGHT order BREAK. else keep checking.
-if both vals are lists:
-    go left to right and compare all values (recursive case).
-    if left side runs out of values first, ✅ RIGHT ORDER. 
-    else if R side runs out first, 🚨 BREAK. WRONG. 
-    if lists are the same length, keep going. 
-if it's list vs. int: 
-    convert the int to a list, 2 becomes [2] 
-    and do the "both vals are lists case." ^^ 
-
-
-TASK: COUNT HOW MANY PAIRS ARE IN THE RIGHT ORDER. 
-RETURN THE SUM OF THEIR INDICES. (first pair index 1)
-"""
-
 import ast 
 
 def main(): 
-    with open('small.txt', 'r') as f:
+    with open('input.txt', 'r') as f:
         lines = f.readlines()
         lines = [line.strip() for line in lines]
         # get a list of pairs 
@@ -59,40 +23,81 @@ def main():
         print(sum([i for i, v in tracker.items () if v==True]))
 
 
+# from the outer loop, a pair is always list vs. list. 
+# returns a definitive true of false 
 def is_right_order(pair):
     left = pair[0]
     right = pair[1]
-    print("Comparing pairs: {} vs. {}".format(left, right))
-    # iterate over left. if left side runs out first, ✅ RIGHT ORDER. 
-    for i, leftItem in enumerate(left): 
-        print("🔄 For loop iteration, i={}".format(i))
+    print("\n🚀 OUTER LOOP: Comparing left: {} to right: {}".format(left, right))
+    for i, lItem in enumerate(left):
+        print("🔄 For loop iteration.") 
         if i >= len(right):
-            # left side still has values, but right side ran out. 
-            print("🚨 right side ran out of values, wrong order!")
-            return False 
-        # otherwise, compare values. 
+            print("🚨 right side ran out of values, wrong order! definite false in outer loop.")
+            return False   
+        rItem = right[i] 
+        print("🔎 Comparing left item: {} to right item: {}".format(lItem, rItem))
+        if isinstance(lItem, int) and isinstance(rItem, int):
+            if lItem < rItem:
+                print("✅ left int < right int. RIGHT ORDER. BREAK.")
+                return True
+            elif lItem > rItem:
+                print("🚨 left int > right int. WRONG ORDER. BREAK.")
+                return False
+            else:
+                continue
+        if isinstance(lItem, int) and isinstance(rItem, list):
+            lItem = [lItem]
+        if isinstance(lItem, list) and isinstance(rItem, int):
+            rItem = [rItem]
+        helperResult = helper([lItem, rItem])
+        if helperResult == -1:
+            return False
+        elif helperResult == 1:
+            return True
+        else: # helper returned 0 
+            continue
+    # "ran out of left items condition"
+    print("✅ ran out of left items, outer loop. BREAK.")
+    return True 
+
+
+# returns -1 if wrong, 1 if right, 0 if unsure 
+def helper(pair):
+    left = pair[0]
+    right = pair[1]
+    # if right is of type list 
+    if isinstance(right, list):
+        if len(right) == 0:
+            return -1
+    if isinstance(left, int) and isinstance(right, list):
+        left = [left]
+    if isinstance(left, int) and isinstance(right, int):
+        if left < right:
+            return 1 
+        elif left > right:
+            return -1  
+        else:
+            return 0 
+    for i, leftItem in enumerate(left):
+        if i >= len(right):
+            return -1
         rightItem = right[i]
-        # are both leftItem and rightItem integers?
         if isinstance(leftItem, int) and isinstance(rightItem, int):
             if leftItem < rightItem:
-                print("✅ left int > right int. RIGHT ORDER. BREAK.")
-                return True
-        # are both values lists? recurse.
-        if isinstance(leftItem, list) and isinstance(rightItem, list):
-            print("Running recursive case on: {} vs. {}".format(leftItem, rightItem))
-            return is_right_order([leftItem, rightItem])
-        # is one a list and the other an int?
+                return 1
+            elif leftItem > rightItem:
+                return -1
+            else:
+                continue
         if isinstance(leftItem, list) and isinstance(rightItem, int):
             rightItem = [rightItem]
-            print("Running recursive case on: {} vs. {}".format(leftItem, rightItem))
-            return is_right_order([leftItem, rightItem])
-        if isinstance(leftItem, int) and isinstance(rightItem, list):
+        if isinstance(leftItem, list) and isinstance(rightItem, int):
             leftItem = [leftItem]
-            print("Running recursive case on: {} vs. {}".format(leftItem, rightItem))
-            return is_right_order([leftItem, rightItem])
-    # if we  get out of the for loop, we went through ALL left values, so return true. 
-    print("✅ left side ran out of values, right order!")
-    return True 
+        return helper([leftItem, rightItem])
+    return 1
+
+
+
 
 # execute main
 if __name__ == '__main__':
---
+++ day-13/wrong-part-one.py
@@ -0,0 +1,102 @@
+"""
+task: reorder a list of packets to decode a message. 
+
+given a list of pairs of packets: how many are in the right order? 
+
+each list starts with [, ends with ] and contains 0 or more comma separated values, either:
+     an integer 
+     or another list 
+
+you could see [7,7,7,7]  ("regular list")
+or [[8,7,6]]  ("nested with many shell layers")
+or [[1],[2,3,4]]  ("nested separate")
+or [1,[2,[3,[4,[5,6,7]]]],8,9]  ("nested populated)
+or [] ("Regular empty list")
+or [[[]]] ("empty nested list")
+
+given a pair that looks like: [1,1,3,1,1] vs [1,1,5,1,1] 
+you iterate left to right and compare 0th to 0th, and so on.  ("left" vs "right")
+
+
+if both vals are ints: 
+    lower int must come before right. so if left < right, ✅ RIGHT order BREAK. else keep checking.
+if both vals are lists:
+    go left to right and compare all values (recursive case).
+    if left side runs out of values first, ✅ RIGHT ORDER. 
+    else if R side runs out first, 🚨 BREAK. WRONG. 
+    if lists are the same length, keep going. 
+if it's list vs. int: 
+    convert the int to a list, 2 becomes [2] 
+    and do the "both vals are lists case." ^^ 
+
+
+TASK: COUNT HOW MANY PAIRS ARE IN THE RIGHT ORDER. 
+RETURN THE SUM OF THEIR INDICES. (first pair index 1)
+"""
+
+import ast 
+
+def main(): 
+    with open('small.txt', 'r') as f:
+        lines = f.readlines()
+        lines = [line.strip() for line in lines]
+        # get a list of pairs 
+        pairs = [] 
+        for i in range(0, len(lines)-1, 3):
+            p0 = ast.literal_eval(lines[i])
+            p1 = ast.literal_eval(lines[i+1])
+            pairs.append([p0, p1]) 
+        # track which are in the right vs. wrong order 
+        tracker = {}
+        for i, pair in enumerate(pairs):
+            print("\n\n 🚀Outer loop on official pair: {} vs. {}".format(pair[0], pair[1]))
+            tracker[i+1] = is_right_order(pair)
+        
+
+        # print the sum of the indices of the right order pairs
+        print("🏁 DONE")
+        print(tracker)
+        print(sum([i for i, v in tracker.items () if v==True]))
+
+
+def is_right_order(pair):
+    left = pair[0]
+    right = pair[1]
+    print("just started recursive func, Comparing pairs: {} vs. {}".format(left, right))
+    # iterate over left. if left side runs out first, ✅ RIGHT ORDER. 
+    for i, leftItem in enumerate(left): 
+        print("🔄 For loop iteration, i={}, left[i] is {}".format(i, leftItem))
+        if i >= len(right):
+            # left side still has values, but right side ran out. 
+            print("🚨 right side ran out of values, wrong order!")
+            return False 
+        # otherwise, compare values. 
+        rightItem = right[i]
+        # are both leftItem and rightItem integers?
+        if isinstance(leftItem, int) and isinstance(rightItem, int):
+            if leftItem < rightItem:
+                print("✅ left int < right int. RIGHT ORDER. BREAK.")
+                return True
+            if leftItem > rightItem:
+                print("🚨 left int > right int. WRONG ORDER. BREAK.")
+                return False
+        # are both values lists? recurse.
+        if isinstance(leftItem, list) and isinstance(rightItem, list):
+            print("Running recursive case on: {} vs. {}".format(leftItem, rightItem))
+            return is_right_order([leftItem, rightItem])
+        # is one a list and the other an int?
+        if isinstance(leftItem, list) and isinstance(rightItem, int):
+            rightItem = [rightItem]
+            print("Running recursive case on: {} vs. {}".format(leftItem, rightItem))
+            return is_right_order([leftItem, rightItem])
+        if isinstance(leftItem, int) and isinstance(rightItem, list):
+            leftItem = [leftItem]
+            print("Running recursive case on: {} vs. {}".format(leftItem, rightItem))
+            return is_right_order([leftItem, rightItem])
+    # if we  get out of the for loop, we went through ALL left values, so return true. 
+    print("✅ left side ran out of values, right order!")
+    return True 
+
+# execute main
+if __name__ == '__main__':
+    main()
